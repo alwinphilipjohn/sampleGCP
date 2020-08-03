f065290ec81c3a508fb10a57f6d1108a7e969676
alwinphilipjohn <alwin.philip.john@gmail.com>
2020-08-03 10:13:15 +0700

    basic files

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+console.log("Server Side running at 5000");
+
+app.listen(5000, () => {
+    console.log("Started Listening")
+});
+
+app.get("/",(req, res) => {
+    res.sendFile(__dirname + "/login.html")
+})
