69500bd9748c7ceb782f2efbe4868d3644bfd2f1
alwinphilipjohn <alwin.philip.john@gmail.com>
2020-08-03 10:24:42 +0700

    basic files corrected

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 console.log("Server Side running at 5000");
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Started Listening")
 });
 
